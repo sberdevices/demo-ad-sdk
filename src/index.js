fa9ce9ae19d236b54728557ff332439b3e81ed6e
Vasiliy Loginevskiy <yeti.or@gmail.com>
2021-10-27 17:16:41 +0300

    feat: new init API

--- src/index.js
+++ src/index.js
@@ -1,69 +1,40 @@
 
-const { createAssistant, createSmartappDebugger } = window.assistant;
-
-const { init, run } = window.SberDevicesVideoAdSDK;
+const { init, initDev, runVideoAd } = window.SberDevicesAdSDK;
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 const DEV_TOKEN = process.env.DEV_TOKEN;
 const DEV_PHRASE = process.env.DEV_PHRASE;
-
-const assistantState = {};
-const initializeAssistant = () => {
-    if (!IS_DEVELOPMENT) {
-        return createAssistant({
-            getState: () => assistantState,
-        });
-    }
-
-    if (!DEV_TOKEN || !DEV_PHRASE) {
-        throw new Error('');
-    }
-
-    return createSmartappDebugger({
-        token: DEV_TOKEN,
-        initPhrase: DEV_PHRASE,
-        getState: () => assistantState,
-    });
-};
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const testBtn = document.querySelector('.btn');
     testBtn.disabled = true;
 
-    const assistant = initializeAssistant();
+    const onSuccess = () => {
+        console.log('AdSdk Inited');
+        testBtn.disabled = false;
+    };
+    const onError = (err) => {
+        console.error('AdSDK Error', err);
+    };
 
-    assistant.on('data', (command) => {
-        if (command.type === 'smart_app_data' && command.smart_app_data.type === 'sub') {
-            try {
-                init(command.smart_app_data.payload);
-                testBtn.disabled = false;
-            } catch(err) {
-                console.error(err);
-            }
-        }
-    });
+    if (IS_DEVELOPMENT) {
+        initDev({ token: DEV_TOKEN, initPhrase: DEV_PHRASE, onSuccess, onError });
+    } else {
+        init({ onSuccess, onError });
+    }
 
     let i = 1;
     const text = testBtn.textContent;
 
     testBtn.addEventListener('click', () => {
-        try {
-            run({
-                onSuccess: () => {
-                    console.log('Success');
-                    testBtn.textContent = text + ': watched ' + i++;
-                },
-                onError: (err) => {
-                    console.log('Error', err);
-                },
-            });
-        } catch(err) {
-            console.error(err);
-        }
+        runVideoAd({
+            onSuccess: () => {
+                console.log('Success');
+                testBtn.textContent = text + ': watched ' + i++;
+            },
+            onError,
+        });
     });
 
 });
-
